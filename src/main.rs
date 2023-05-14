1bfd7c2448cb4047e495a0a2a54ba7fcc58a81c4
t4ccer <t4ccer@gmail.com>
2023-05-13 18:14:30 -0600

    Remove numbers from structure

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
-use std::fmt::Display;
+use std::{fmt::Display, fs::remove_dir, ops::Not};
 
-use nom::{bytes::complete::tag, character::complete, multi::separated_list0, IResult};
+use nom::{bytes::complete::tag, multi::separated_list0, IResult};
 
 // TODO: Find rational library
 type Num = i64;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum Value {
-    Number(Num),
-    Star(Num),
-}
-
 mod parser {
+    use nom::{
+        character::complete::{self, multispace0},
+        IResult, Parser,
+    };
 
-    use nom::{bytes::complete::tag, character::complete::multispace0, IResult, Parser};
-
-    pub fn star(input: &str) -> IResult<&str, ()> {
-        tag("*")(input).map(|(input, _)| (input, ()))
+    pub fn i64(input: &str) -> IResult<&str, i64> {
+        complete::i64(input)
     }
 
     pub fn lexeme<'a, Output, Error, F>(
@@ -35,171 +30,192 @@ mod parser {
         }
     }
 }
-impl Value {
-    fn parse(input: &str) -> IResult<&str, Self> {
-        let (input, num) = complete::i64(input)?;
-
-        match parser::star(input) {
-            Ok((input, _star)) => Ok((input, Value::Star(num))),
-            Err(_) => Ok((input, Value::Number(num))),
-        }
-    }
-}
-
-#[test]
-fn parse_num_value() {
-    let inp = "42";
-    let exp = ("", Value::Number(42));
-    assert_eq!(exp, Value::parse(inp).unwrap());
-}
-
-#[test]
-fn parse_star_value() {
-    let inp = "42*";
-    let exp = ("", Value::Star(42));
-    assert_eq!(exp, Value::parse(inp).unwrap());
-}
-
-impl Display for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Value::Number(n) => write!(f, "{}", n),
-            Value::Star(n) => write!(f, "{}*", n),
-        }
-    }
-}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum Game {
-    Value(Value),
-    Moves(Vec<Game>, Vec<Game>),
+pub struct Game {
+    pub left: Vec<Game>,
+    pub right: Vec<Game>,
 }
 
 impl Game {
     fn parse(input: &str) -> IResult<&str, Self> {
-        match Value::parse(input) {
-            Ok((input, val)) => Ok((input, Game::Value(val))),
-            Err(_) => {
-                let (input, _) = parser::lexeme(tag("{"))(input)?;
-                let (input, left) =
-                    separated_list0(parser::lexeme(tag(",")), parser::lexeme(Game::parse))(input)?;
-                let (input, _) = parser::lexeme(tag("|"))(input)?;
-                let (input, right) =
-                    separated_list0(parser::lexeme(tag(",")), parser::lexeme(Game::parse))(input)?;
-                let (input, _) = parser::lexeme(tag("}"))(input)?;
-                Ok((input, Game::Moves(left, right)))
-            }
+        let (input, _) = parser::lexeme(tag("{"))(input)?;
+        let (input, left) =
+            separated_list0(parser::lexeme(tag(",")), parser::lexeme(Game::parse))(input)?;
+        let (input, _) = parser::lexeme(tag("|"))(input)?;
+        let (input, right) =
+            separated_list0(parser::lexeme(tag(",")), parser::lexeme(Game::parse))(input)?;
+        let (input, _) = parser::lexeme(tag("}"))(input)?;
+        Ok((input, Game { left, right }))
+    }
+
+    fn zero() -> Game {
+        Game {
+            left: vec![],
+            right: vec![],
         }
     }
-}
 
-#[test]
-fn parse_value_game() {
-    let inp = "42";
-    let exp = ("", Game::Value(Value::Number(42)));
-    assert_eq!(exp, Game::parse(inp).unwrap());
+    fn one() -> Game {
+        Game {
+            left: vec![Game::zero()],
+            right: vec![],
+        }
+    }
 }
 
 #[test]
 fn parse_empty_game() {
     let inp = "{|}";
-    let exp = ("", Game::Moves(vec![], vec![]));
+    let exp = ("", Game::zero());
     assert_eq!(exp, Game::parse(inp).unwrap());
 }
 
 #[test]
 fn parse_left_game() {
-    let inp = "{1,2*,3*|}";
+    let inp = "{{|},{|}|}";
     let exp = (
         "",
-        Game::Moves(
-            vec![
-                Game::Value(Value::Number(1)),
-                Game::Value(Value::Star(2)),
-                Game::Value(Value::Star(3)),
-            ],
-            vec![],
-        ),
+        Game {
+            left: vec![Game::zero(), Game::zero()],
+            right: vec![],
+        },
     );
     assert_eq!(exp, Game::parse(inp).unwrap());
 }
 
 #[test]
 fn parse_right_game() {
-    let inp = "{|42*  ,  21}";
+    let inp = "{|{|}}";
     let exp = (
         "",
-        Game::Moves(
-            vec![],
-            vec![Game::Value(Value::Star(42)), Game::Value(Value::Number(21))],
-        ),
+        Game {
+            left: vec![],
+            right: vec![Game::zero()],
+        },
     );
     assert_eq!(exp, Game::parse(inp).unwrap());
 }
 
 #[test]
 fn parse_nested_game() {
-    let inp = "{{1 , 2* | 3*, 4}| { 0 | 0 }}";
+    let inp = "{ {|} | {|} }";
     let exp = (
         "",
-        Game::Moves(
-            vec![Game::Moves(
-                vec![Game::Value(Value::Number(1)), Game::Value(Value::Star(2))],
-                vec![Game::Value(Value::Star(3)), Game::Value(Value::Number(4))],
-            )],
-            vec![Game::Moves(
-                vec![Game::Value(Value::Number(0))],
-                vec![Game::Value(Value::Number(0))],
-            )],
-        ),
+        Game {
+            left: vec![Game::zero()],
+            right: vec![Game::zero()],
+        },
     );
     assert_eq!(exp, Game::parse(inp).unwrap());
 }
 
 impl Display for Game {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Game::Value(v) => write!(f, "{}", v),
-            Game::Moves(l, r) => {
-                write!(f, "{{")?;
-                for (idx, m) in l.iter().enumerate() {
-                    if idx != 0 {
-                        write!(f, ",")?;
-                    }
-                    write!(f, "{}", *m)?;
-                }
-                write!(f, "|")?;
-                for (idx, m) in r.iter().enumerate() {
-                    if idx != 0 {
-                        write!(f, ",")?;
-                    }
-                    write!(f, "{}", *m)?;
-                }
-                write!(f, "}}")?;
-                Ok(())
+        write!(f, "{{")?;
+        for (idx, m) in self.left.iter().enumerate() {
+            if idx != 0 {
+                write!(f, ",")?;
             }
+            write!(f, "{}", *m)?;
         }
+        write!(f, "|")?;
+        for (idx, m) in self.right.iter().enumerate() {
+            if idx != 0 {
+                write!(f, ",")?;
+            }
+            write!(f, "{}", *m)?;
+        }
+        write!(f, "}}")?;
+        Ok(())
     }
 }
 
 impl Game {
-    fn canonical_form(self) -> Self {
-        match self {
-            Game::Value(n) => Game::Value(n),
-            // Empty game
-            Game::Moves(left, right) if left.is_empty() && right.is_empty() => {
-                Game::Value(Value::Number(0))
+    fn greater_eq(lhs: &Game, rhs: &Game) -> bool {
+        Self::less_eq(rhs, lhs)
+    }
+
+    fn less_eq(g: &Game, h: &Game) -> bool {
+        g.left.iter().any(|gl| Self::less_eq(h, gl)).not()
+            && h.right.iter().any(|hr| Self::less_eq(hr, g)).not()
+    }
+
+    fn confused(lhs: &Game, rhs: &Game) -> bool {
+        !(Self::less_eq(lhs, rhs)) && !(Self::less_eq(rhs, lhs))
+    }
+
+    fn remove_dominated_by<F>(comp: F, games: &[Game]) -> Vec<Game>
+    where
+        F: Fn(&Game, &Game) -> bool,
+    {
+        match games.get(0) {
+            None => vec![],
+            Some(g) => {
+                // TODO: refactor
+                let gs = &games[1..];
+                if gs.iter().any(|gg| comp(gg, g)) {
+                    Self::remove_dominated_by(comp, gs)
+                } else {
+                    let gs: Vec<Game> = gs
+                        .iter()
+                        .filter(|gg| Self::confused(g, gg))
+                        .cloned()
+                        .collect();
+                    let mut res = Self::remove_dominated_by(comp, &gs);
+                    res.push(g.clone());
+                    res
+                }
             }
-            _ => self,
         }
+    }
+
+    fn remove_dominated(&self) -> Game {
+        Game {
+            left: Self::remove_dominated_by(Self::greater_eq, &self.left),
+            right: Self::remove_dominated_by(Self::less_eq, &self.right),
+        }
+    }
+
+    pub fn canonical_form(self) -> Self {
+        let g = self.remove_dominated();
+        todo!()
     }
 }
 
-fn main() {
-    let g = Game::parse("{|}").unwrap().1;
-    println!("{}", g);
+#[test]
+fn zero_leq_zero() {
+    assert!(Game::less_eq(&Game::zero(), &Game::zero()))
+}
 
-    let g = g.canonical_form();
-    println!("{}", g);
+#[test]
+fn zero_leq_one() {
+    assert!(Game::less_eq(&Game::zero(), &Game::one()))
+}
+
+#[test]
+fn one_dominates_zero() {
+    assert_eq!(
+        Game {
+            left: vec![Game::zero(), Game::one()],
+            right: vec![]
+        }
+        .remove_dominated(),
+        Game {
+            left: vec![Game::one()],
+            right: vec![]
+        }
+    )
+}
+
+fn main() {
+    // let g = Game::parse("{{{|}|},{|}|}").unwrap().1;
+    // println!("{}", g);
+
+    // let g = g.remove_dominated();
+    // println!("{}", g);
+
+    println!("{}", Game::zero());
+    println!("{}", Game::one());
 }
