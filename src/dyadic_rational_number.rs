4959fd94f7542c725a04cc5a0ba16c8352c79914
t4ccer <t4ccer@gmail.com>
2023-05-21 17:21:45 -0600

    Add more dyadic utils

--- src/dyadic_rational_number.rs
+++ src/dyadic_rational_number.rs
@@ -1,3 +1,7 @@
+use std::ops::{Add, AddAssign, DivAssign};
+
+use gcd::Gcd;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct DyadicRationalNumber {
     numerator: i32,
@@ -17,15 +21,58 @@ impl DyadicRationalNumber {
         self.denominator().trailing_zeros() as i32
     }
 
+    fn normalized(&self) -> Self {
+        let d = Gcd::gcd(
+            self.numerator().abs() as u32,
+            self.denominator().abs() as u32,
+        );
+
+        DyadicRationalNumber {
+            numerator: self.numerator / (d as i32),
+            denominator: self.denominator / (d as i32),
+        }
+    }
+
+    fn normalize(&mut self) {
+        let d = Gcd::gcd(
+            self.numerator().abs() as u32,
+            self.denominator().abs() as u32,
+        );
+
+        self.numerator.div_assign(d as i32);
+        self.denominator.div_assign(d as i32);
+    }
+
     pub fn rational(numerator: i32, denominator: i32) -> Option<Self> {
         if denominator == 0 {
             return None;
         }
+        if numerator == 0 {
+            return Some(DyadicRationalNumber {
+                numerator: 0,
+                denominator: 1,
+            });
+        }
+
+        let sign = numerator.signum() * denominator.signum();
+
         // FIXME: Check if fraction is dyadic
-        Some(DyadicRationalNumber {
-            numerator,
-            denominator,
-        })
+        Some(
+            DyadicRationalNumber {
+                numerator: numerator.abs() * sign,
+                denominator: denominator.abs(),
+            }
+            .normalized(),
+        )
+    }
+
+    /// Convert to intger if it's an integer
+    pub fn to_integer(&self) -> Option<i32> {
+        if self.denominator == 1 {
+            Some(self.numerator)
+        } else {
+            None
+        }
     }
 }
 
@@ -35,6 +82,26 @@ impl From<i32> for DyadicRationalNumber {
             numerator: value,
             denominator: 1,
         }
+    }
+}
+
+impl Add for DyadicRationalNumber {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        DyadicRationalNumber {
+            numerator: self.numerator() * rhs.denominator + self.denominator * rhs.numerator,
+            denominator: self.denominator() * rhs.denominator(),
+        }
+        .normalized()
+    }
+}
+
+impl AddAssign for DyadicRationalNumber {
+    fn add_assign(&mut self, rhs: Self) {
+        self.numerator = self.numerator() * rhs.denominator + self.denominator * rhs.numerator;
+        self.denominator = self.denominator() * rhs.denominator();
+        self.normalize();
     }
 }
 
