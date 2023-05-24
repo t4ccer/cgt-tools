020b411232ff39cb0ad49ca49300b9ad7ef9b80f
t4ccer <t4ccer@gmail.com>
2023-05-23 23:06:45 -0600

    Create simple executable

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,80 @@
+use anyhow::anyhow;
+use anyhow::Result;
 use cgt::domineering::Grid;
 use cgt::short_canonical_game::GameBackend;
+use clap::Parser;
 
-fn main() {
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Domineering grid width
+    #[arg(long)]
+    width: usize,
+
+    /// Domineering grid height
+    #[arg(long)]
+    height: usize,
+
+    /// Starting position id
+    #[arg(long, default_value_t = 0)]
+    start_id: usize,
+
+    /// Last position id
+    #[arg(long, default_value = None)]
+    last_id: Option<usize>,
+
+    /// How often to log progress
+    #[arg(long, default_value_t = 1000)]
+    progress_step: usize,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let grid_tiles = args.width * args.height;
+    let max_grid_tiles = 8 * std::mem::size_of::<usize>();
+
+    if grid_tiles > max_grid_tiles {
+        Err(anyhow!(
+            "Size of grid (width * height) is {}, but it cannot exceed {}",
+            grid_tiles,
+            max_grid_tiles
+        ))?;
+    }
+
+    let max_last_id = 1 << grid_tiles;
+
+    let last_id: usize = match args.last_id {
+        None => max_last_id,
+        Some(last_id) => last_id,
+    };
+
+    if last_id > max_last_id {
+        Err(anyhow!(
+            "last-id is {}, but for this grid it cannot exceed {}",
+            last_id,
+            max_last_id
+        ))?;
+    }
+
+    let total_len: u32 = last_id.ilog10() + 1;
+
     let mut b = GameBackend::new();
-    let width: usize = 4;
-    let height: usize = 4;
-    let total: usize = 1 << (width * height);
-    let total_len: u32 = total.ilog10() + 1;
-
-    for i in 0usize..total {
-        let mut grid_arr = vec![false; width * height];
-        for grid_idx in 0..(width * height) {
+    for i in args.start_id..last_id {
+        let mut grid_arr = vec![false; grid_tiles];
+        for grid_idx in 0..(args.width * args.height) {
             grid_arr[grid_idx] = ((i >> grid_idx) & 1) == 1;
         }
-        let grid = Grid::from_arr(width, height, &grid_arr);
+        let grid = Grid::from_arr(args.width, args.height, &grid_arr);
         let game = grid.canonical_form(&mut b);
-        if i % 1000 == 0 || i == total - 1 {
+        if i % args.progress_step == 0 || i == last_id - 1 {
             let progress = format!("{}", i);
             let pad_len = total_len - (progress.len() as u32);
             let pad = "0".repeat(pad_len as usize);
-            eprintln!("{}{}/{}", pad, progress, total - 1);
+            eprintln!("{}{}/{}", pad, progress, last_id - 1);
         }
         println!("{}\n{}\n", grid, b.dump_game_to_str(game));
     }
+
+    Ok(())
 }
