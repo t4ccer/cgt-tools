faf39439bbc19508aa4c797bd71072bf95b77b14
t4ccer <t4ccer@gmail.com>
2023-08-19 22:08:06 +0200

    Enable `clippy` in CI

--- flake.nix
+++ flake.nix
@@ -32,9 +32,16 @@
         self',
         inputs',
         pkgs,
+        lib,
         system,
         ...
-      }: {
+      }: let
+        rustToolchain = pkgs.rust-bin.fromRustupToolchain {
+          channel = "stable";
+          components = ["rust-analyzer" "rust-src" "rustfmt" "rustc" "cargo"];
+          targets = ["x86_64-unknown-linux-gnu" "wasm32-unknown-unknown"];
+        };
+      in {
         _module.args.pkgs = import self.inputs.nixpkgs {
           inherit system;
           overlays = [inputs.rust-overlay.overlays.rust-overlay];
@@ -44,8 +51,12 @@
           src = ./.;
           hooks = {
             alejandra.enable = true;
-            statix.enable = true;
             rustfmt.enable = true;
+            clippy.enable = true;
+          };
+          tools = {
+            rustfmt = lib.mkForce rustToolchain;
+            clippy = lib.mkForce rustToolchain;
           };
         };
 
@@ -56,23 +67,18 @@
           '';
           nativeBuildInputs = [
             pkgs.alejandra
-            pkgs.fd
             pkgs.cargo-flamegraph
-            pkgs.cargo-tarpaulin
-            pkgs.cargo-modules
             pkgs.cargo-leptos
-            pkgs.trunk
+            pkgs.cargo-modules
+            pkgs.cargo-tarpaulin
+            pkgs.fd
+            pkgs.graphviz
+            pkgs.heaptrack
             pkgs.nodePackages.tailwindcss
             pkgs.sage
-            pkgs.heaptrack
             pkgs.texlive.combined.scheme-full
-            pkgs.graphviz
-
-            (pkgs.rust-bin.fromRustupToolchain {
-              channel = "stable";
-              components = ["rust-analyzer" "rust-src" "rustfmt" "rustc" "cargo"];
-              targets = ["x86_64-unknown-linux-gnu" "wasm32-unknown-unknown"];
-            })
+            pkgs.trunk
+            rustToolchain
           ];
         };
         formatter = pkgs.alejandra;
