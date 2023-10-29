3b23d1147df1fb7912254ab7bedf1fa4e0916245
t4ccer <t4ccer@gmail.com>
2023-10-28 22:24:48 -0600

    Split python bindings into modules

---
+++ cgt-py/src/canonical_form.rs
@@ -0,0 +1,67 @@
+use cgt::short::partizan::canonical_form::CanonicalForm;
+use pyo3::{prelude::*, pyclass::CompareOp};
+use std::{
+    ops::{Add, Neg, Sub},
+    str::FromStr,
+};
+
+#[pyclass(name = "CanonicalForm")]
+#[derive(Clone)]
+pub struct PyCanonicalForm {
+    inner: CanonicalForm,
+}
+
+impl From<CanonicalForm> for PyCanonicalForm {
+    fn from(canonical_form: CanonicalForm) -> Self {
+        Self {
+            inner: canonical_form,
+        }
+    }
+}
+
+#[pymethods]
+impl PyCanonicalForm {
+    #[new]
+    fn py_new(value: &PyAny) -> PyResult<Self> {
+        if let Ok(integer) = value.extract::<i64>() {
+            return Ok(Self::from(CanonicalForm::new_integer(integer)));
+        } else if let Ok(string) = value.extract::<&str>() {
+            match CanonicalForm::from_str(string) {
+                Ok(cf) => return Ok(Self::from(cf)),
+                Err(_) => {
+                    return Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(
+                        "Could not parse CanonicalForm. Invalid input format.",
+                    ))
+                }
+            }
+        } else if let Ok(canonical_form) = value.extract::<PyCanonicalForm>() {
+            return Ok(canonical_form);
+        }
+
+        Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(
+            "Could not convert to CanonicalForm. Expected integer or string.",
+        ))
+    }
+
+    fn __repr__(&self) -> String {
+        format!("CanonicalForm('{}')", self.inner)
+    }
+
+    fn __add__(&self, other: &Self) -> Self {
+        Self::from(Add::add(&self.inner, &other.inner))
+    }
+
+    fn __sub__(&self, other: &Self) -> Self {
+        Self::from(Sub::sub(&self.inner, &other.inner))
+    }
+
+    fn __neg__(&self) -> Self {
+        Self::from(Neg::neg(&self.inner))
+    }
+
+    fn __richcmp__(&self, other: &Self, op: CompareOp) -> bool {
+        self.inner
+            .partial_cmp(&other.inner)
+            .map_or(false, |ord| op.matches(ord))
+    }
+}
--- cgt-py/src/lib.rs
+++ cgt-py/src/lib.rs
@@ -1,123 +1,13 @@
-use cgt::{numeric::nimber::Nimber, short::partizan::canonical_form::CanonicalForm};
-use pyo3::{prelude::*, pyclass::CompareOp};
-use std::{
-    ops::{Add, Neg, Sub},
-    str::FromStr,
-};
+use pyo3::prelude::*;
+
+mod canonical_form;
+mod nimber;
+
+use crate::canonical_form::*;
+use crate::nimber::*;
 
 // TODO: Pretty printers
 // TODO: SVG rendering & html()
-
-#[pyclass(name = "Nimber")]
-#[derive(Clone)]
-struct PyNimber {
-    inner: Nimber,
-}
-
-impl From<Nimber> for PyNimber {
-    fn from(nimber: Nimber) -> Self {
-        Self { inner: nimber }
-    }
-}
-
-#[pymethods]
-impl PyNimber {
-    #[new]
-    fn py_new(value: u32) -> Self {
-        PyNimber::from(Nimber::new(value))
-    }
-
-    fn __repr__(&self) -> String {
-        format!("Nimber({})", self.inner.value())
-    }
-
-    fn __add__(&self, other: &Self) -> Self {
-        Self::from(Add::add(&self.inner, &other.inner))
-    }
-
-    fn __sub__(&self, other: &Self) -> Self {
-        Self::from(Sub::sub(&self.inner, &other.inner))
-    }
-
-    fn __neg__(&self) -> Self {
-        Self::from(Neg::neg(&self.inner))
-    }
-
-    fn __richcmp__(&self, other: &Self, op: CompareOp) -> bool {
-        op.matches(self.inner.cmp(&other.inner))
-    }
-}
-
-#[pyfunction]
-fn mex(nimbers: Vec<PyNimber>) -> PyNimber {
-    PyNimber::from(Nimber::mex(
-        nimbers
-            .into_iter()
-            .map(|py_nimber| py_nimber.inner)
-            .collect(),
-    ))
-}
-
-#[pyclass(name = "CanonicalForm")]
-#[derive(Clone)]
-struct PyCanonicalForm {
-    inner: CanonicalForm,
-}
-
-impl From<CanonicalForm> for PyCanonicalForm {
-    fn from(canonical_form: CanonicalForm) -> Self {
-        Self {
-            inner: canonical_form,
-        }
-    }
-}
-
-#[pymethods]
-impl PyCanonicalForm {
-    #[new]
-    fn py_new(value: &PyAny) -> PyResult<Self> {
-        if let Ok(integer) = value.extract::<i64>() {
-            return Ok(Self::from(CanonicalForm::new_integer(integer)));
-        } else if let Ok(string) = value.extract::<&str>() {
-            match CanonicalForm::from_str(string) {
-                Ok(cf) => return Ok(Self::from(cf)),
-                Err(_) => {
-                    return Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(
-                        "Could not parse CanonicalForm. Invalid input format.",
-                    ))
-                }
-            }
-        } else if let Ok(canonical_form) = value.extract::<PyCanonicalForm>() {
-            return Ok(canonical_form);
-        }
-
-        Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(
-            "Could not convert to CanonicalForm. Expected integer or string.",
-        ))
-    }
-
-    fn __repr__(&self) -> String {
-        format!("CanonicalForm('{}')", self.inner)
-    }
-
-    fn __add__(&self, other: &Self) -> Self {
-        Self::from(Add::add(&self.inner, &other.inner))
-    }
-
-    fn __sub__(&self, other: &Self) -> Self {
-        Self::from(Sub::sub(&self.inner, &other.inner))
-    }
-
-    fn __neg__(&self) -> Self {
-        Self::from(Neg::neg(&self.inner))
-    }
-
-    fn __richcmp__(&self, other: &Self, op: CompareOp) -> bool {
-        self.inner
-            .partial_cmp(&other.inner)
-            .map_or(false, |ord| op.matches(ord))
-    }
-}
 
 #[pymodule]
 fn cgt_py(_py: Python, m: &PyModule) -> PyResult<()> {
