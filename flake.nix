755fbe5f1c3b50bbe8ad3ab39cae6c5982e1d2c8
t4ccer <t4ccer@gmail.com>
2024-09-15 19:27:23 -0600

    Set up nix based build system

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "cgrs";
+  description = "cgt-tools";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts = {
@@ -48,6 +48,102 @@
         };
 
         pythonToolchain = "python311";
+
+        LD_LIBRARY_PATH = lib.makeLibraryPath [
+          pkgs.libGL
+          pkgs.xorg.libXrandr
+          pkgs.xorg.libXinerama
+          pkgs.xorg.libXcursor
+          pkgs.xorg.libXi
+        ];
+
+        env = {
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS = "-L${pkgs.pkgsCross.mingwW64.windows.mingw_w64_pthreads}/lib";
+          CC_x86_64_pc_windows_gnu = "x86_64-w64-mingw32-gcc";
+          CXX_x86_64_pc_windows_gnu = "x86_64-w64-mingw32-g++";
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "x86_64-w64-mingw32-gcc";
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          inherit LD_LIBRARY_PATH;
+        };
+
+        mkCgtTools = {
+          pkgs,
+          target,
+        }:
+          (pkgs.rustPlatform.buildRustPackage {
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            inherit env;
+
+            nativeBuildInputs = [
+              rustToolchain
+              pkgs.${pythonToolchain}
+              pkgs.pkgsCross.mingwW64.stdenv.cc
+              pkgs.pkg-config
+              pkgs.cmake
+              pkgs.clang
+              pkgs.makeWrapper
+            ];
+
+            buildInputs = [
+              pkgs.wayland-scanner
+              pkgs.xorg.libX11
+              pkgs.xorg.libXrandr
+              pkgs.xorg.libXinerama
+              pkgs.xorg.libXcursor
+              pkgs.mesa
+              pkgs.xorg.libXi
+              pkgs.glfw3
+              pkgs.wayland
+              pkgs.libxkbcommon
+            ];
+          })
+          .overrideAttrs (_:
+            {
+              name = "cgt-tools-${target}";
+
+              buildPhase = ''
+                cargo build -j "$NIX_BUILD_CORES" --target "${target}" --offline --profile release -p cgt_cli
+                cargo build -j "$NIX_BUILD_CORES" --target "${target}" --offline --profile release -p cgt_gui ${
+                  if target == "x86_64-pc-windows-gnu"
+                  then "--no-default-features"
+                  else ""
+                }
+              '';
+
+              installPhase = ''
+                mkdir -p "$out/bin"
+                mv "target/${target}/release/cgt-gui${
+                  if target == "x86_64-pc-windows-gnu"
+                  then ".exe"
+                  else ""
+                }" "$out/bin"
+                mv "target/${target}/release/cgt-cli${
+                  if target == "x86_64-pc-windows-gnu"
+                  then ".exe"
+                  else ""
+                }" "$out/bin"
+                ${
+                  if target == "x86_64-unknown-linux-gnu"
+                  then "wrapProgram $out/bin/cgt-gui --prefix LD_LIBRARY_PATH : ${LD_LIBRARY_PATH}"
+                  else ""
+                }
+              '';
+            }
+            // (pkgs.lib.optionalAttrs (target == "x86_64-pc-windows-gnu") {
+              checkPhase = "true";
+            }));
+
+        pkgsToolchain = import inputs.nixpkgs {
+          inherit system;
+          overyalys = [
+            (final: prev: {
+              rustc = rustToolchain;
+              cargo = rustToolchain;
+            })
+          ];
+        };
       in {
         _module.args.pkgs = import self.inputs.nixpkgs {
           inherit system;
@@ -65,6 +161,18 @@
           tools = {
             rustfmt = lib.mkForce rustToolchain;
             clippy = lib.mkForce rustToolchain;
+          };
+        };
+
+        packages = {
+          cgt-tools-windows = mkCgtTools {
+            pkgs = pkgsToolchain;
+            target = "x86_64-pc-windows-gnu";
+          };
+
+          cgt-tools-linux = mkCgtTools {
+            pkgs = pkgsToolchain;
+            target = "x86_64-unknown-linux-gnu";
           };
         };
 
@@ -115,20 +223,7 @@
             rustToolchain
           ];
 
-          env = {
-            CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS = "-L${pkgs.pkgsCross.mingwW64.windows.mingw_w64_pthreads}/lib";
-            CC_x86_64_pc_windows_gnu = "x86_64-w64-mingw32-gcc";
-            CXX_x86_64_pc_windows_gnu = "x86_64-w64-mingw32-g++";
-            CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "x86_64-w64-mingw32-gcc";
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            LD_LIBRARY_PATH = lib.makeLibraryPath [
-              pkgs.libGL
-              pkgs.xorg.libXrandr
-              pkgs.xorg.libXinerama
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXi
-            ];
-          };
+          inherit env;
         };
         formatter = pkgs.alejandra;
       };
