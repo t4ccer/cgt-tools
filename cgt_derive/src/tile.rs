b6226510303f372bd4caee455f2ce4abe34826c3
t4ccer <t4ccer@gmail.com>
2024-12-30 11:03:18 -0700

    Improve compile-time errors reported by `Tile` derive macro
    We no longer panic but instead we emit dummy trait implementations with
    `unimplemented!()` as body, so there is only one error (the one reported by us)
    instead of lots of errors about tile traits being not implemented because
    the macro panicked

--- cgt_derive/src/tile.rs
+++ cgt_derive/src/tile.rs
@@ -1,5 +1,8 @@
+use std::collections::HashSet;
+
 use proc_macro::TokenStream;
-use quote::quote;
+use proc_macro2::Span;
+use quote::{quote, quote_spanned};
 use syn::{parenthesized, parse_macro_input, Data, DeriveInput, Ident, LitBool, LitChar, Variant};
 
 const TILE_ATTR: &str = "tile";
@@ -9,15 +12,20 @@ const TILE_ATTR_BOOL: &str = "bool";
 
 struct TileAttr {
     ident: Ident,
-    is_default: bool,
-    tile_char: Option<char>,
-    tile_bool: Option<bool>,
+    tile_default: Option<Span>,
+    tile_char: Option<(char, Span)>,
+    tile_bool: Option<(bool, Span)>,
 }
 
-fn to_tile_attr(variant: Variant) -> TileAttr {
+struct Error {
+    span: Span,
+    msg: String,
+}
+
+fn to_tile_attr(variant: Variant) -> Result<TileAttr, Error> {
     let mut tile = TileAttr {
         ident: variant.ident,
-        is_default: false,
+        tile_default: None,
         tile_char: None,
         tile_bool: None,
     };
@@ -33,209 +41,311 @@ fn to_tile_attr(variant: Variant) -> TileAttr {
             }
 
             attr.parse_nested_meta(|meta| {
-                if meta.path.is_ident(TILE_ATTR_DEFAULT) {
-                    tile.is_default = true;
-                    return Ok(());
-                }
+                if let Some(path) = meta.path.get_ident() {
+                    if path == TILE_ATTR_DEFAULT {
+                        tile.tile_default = Some(path.span());
+                        return Ok(());
+                    }
 
-                if meta.path.is_ident(TILE_ATTR_CHAR) {
-                    let content;
-                    parenthesized!(content in meta.input);
-                    let lit: LitChar = content.parse()?;
-                    tile.tile_char = Some(lit.value());
-                    return Ok(());
-                }
+                    if path == TILE_ATTR_CHAR {
+                        let content;
+                        parenthesized!(content in meta.input);
+                        let span = content.span();
+                        let lit: LitChar = content.parse()?;
+                        tile.tile_char = Some((lit.value(), span));
+                        return Ok(());
+                    }
 
-                if meta.path.is_ident(TILE_ATTR_BOOL) {
-                    let content;
-                    parenthesized!(content in meta.input);
-                    let lit: LitBool = content.parse()?;
-                    tile.tile_bool = Some(lit.value());
-                    return Ok(());
+                    if path == TILE_ATTR_BOOL {
+                        let content;
+                        parenthesized!(content in meta.input);
+                        let span = content.span();
+                        let lit: LitBool = content.parse()?;
+                        tile.tile_bool = Some((lit.value(), span));
+                        return Ok(());
+                    }
                 }
 
                 Err(meta.error(format!(
                     "Invalid attribute: '{}'",
-                    meta.path.get_ident().unwrap()
+                    meta.path
+                        .get_ident()
+                        .map_or_else(|| String::from("<UNKNOWN>"), ToString::to_string)
                 )))
             })
-            .unwrap_or_else(|err| panic!("{}", err));
+            .map_err(|err| Error {
+                span: err.span(),
+                msg: format!("{err}"),
+            })?;
         }
     }
 
-    tile
+    Ok(tile)
 }
 
 pub fn derive(input: TokenStream) -> TokenStream {
-    // TODO: Detect external usage and use '::cgt' instead
-    // maybe via bootstrap internal feature
-    let cgt_crate = quote! {crate};
+    let crate_name = std::env::var("CARGO_PKG_NAME").unwrap();
+    let cgt_crate = if crate_name == "cgt" {
+        quote! {crate}
+    } else {
+        quote! {crate_name}
+    };
 
     let ast = parse_macro_input!(input as DeriveInput);
 
-    let Data::Enum(tile_enum) = ast.data else {
-        panic!("'Tile' derive macro can be used only on enum.")
-    };
     let tile_enum_name = ast.ident;
 
-    let tiles = tile_enum
+    let dummy_impls = quote! {
+        #[automatically_derived]
+        impl ::std::default::Default for #tile_enum_name {
+            #[inline]
+            fn default() -> Self {
+                unimplemented!()
+            }
+        }
+
+        #[automatically_derived]
+        impl #cgt_crate::grid::BitTile for #tile_enum_name {
+            #[inline]
+            fn tile_to_bool(self) -> bool {
+                unimplemented!()
+            }
+
+
+            #[inline]
+            fn bool_to_tile(_input: bool) -> Self {
+                unimplemented!()
+            }
+        }
+
+        #[automatically_derived]
+        impl #cgt_crate::grid::CharTile for #tile_enum_name {
+            #[inline]
+            fn tile_to_char(self) -> char {
+                unimplemented!()
+            }
+
+            #[inline]
+            fn char_to_tile(_input: char) -> Option<Self> {
+                unimplemented!()
+            }
+        }
+    };
+
+    let Data::Enum(tile_enum) = ast.data else {
+        return quote! {
+            #dummy_impls
+            compile_error!("'Tile' derive macro can be used only on enum");
+        }
+        .into();
+    };
+
+    match tile_enum
         .variants
         .into_iter()
         .map(to_tile_attr)
-        .collect::<Vec<_>>();
-
-    let impl_default = {
-        let default_tiles = tiles
-            .iter()
-            .filter(|tile| tile.is_default)
-            .collect::<Vec<_>>();
-        match &default_tiles[..] {
-            [tile] => {
-                let constr = &tile.ident;
-                Some(quote! {
-                    #[automatically_derived]
-                    impl ::std::default::Default for #tile_enum_name {
-                        #[inline]
-                        fn default() -> Self {
-                            #tile_enum_name::#constr
-                        }
-                    }
-                })
+        .collect::<Result<Vec<_>, _>>()
+    {
+        Err(err) => {
+            let msg = err.msg;
+            quote_spanned! { err.span =>
+                #dummy_impls
+                compile_error!(#msg);
             }
-            [] => None,
-            _ => panic!("Only one tile can be default"),
+            .into()
         }
-    };
-
-    let impl_char_tile = {
-        let mut without_char = 0;
-
-        let tile_to_chars = tiles
-            .iter()
-            .filter_map(|tile| {
-                tile.tile_char.map_or_else(
-                    || {
-                        without_char += 1;
-                        None
-                    },
-                    |c| {
+        Ok(tiles) => {
+            let impl_default = {
+                let default_tiles = tiles
+                    .iter()
+                    .filter(|tile| tile.tile_default.is_some())
+                    .collect::<Vec<_>>();
+                match &default_tiles[..] {
+                    [tile] => {
                         let constr = &tile.ident;
                         Some(quote! {
-                            #tile_enum_name::#constr => #c
+                            #[automatically_derived]
+                            impl ::std::default::Default for #tile_enum_name {
+                                #[inline]
+                                fn default() -> Self {
+                                    #tile_enum_name::#constr
+                                }
+                            }
                         })
-                    },
-                )
-            })
-            .collect::<Vec<_>>();
+                    }
+                    [] => None,
+                    [_, tile, ..] => {
+                        let span = tile.tile_default.unwrap();
+                        return quote_spanned! { span =>
+                            #dummy_impls
+                            compile_error!("Only one tile can be default");
+                        }
+                        .into();
+                    }
+                }
+            };
 
-        let char_to_tiles = tiles
-            .iter()
-            .filter_map(|tile| {
-                tile.tile_char.map(|c| {
-                    let constr = &tile.ident;
-                    Some(quote! {
-                        #c => ::core::option::Option::Some(#tile_enum_name::#constr)
+            let impl_char_tile = {
+                let mut without_char = Vec::new();
+
+                let tile_to_chars = tiles
+                    .iter()
+                    .filter_map(|tile| {
+                        tile.tile_char.map_or_else(
+                            || {
+                                without_char.push(tile.ident.span());
+                                None
+                            },
+                            |(c, _)| {
+                                let constr = &tile.ident;
+                                Some(quote! {
+                                    #tile_enum_name::#constr => #c
+                                })
+                            },
+                        )
                     })
-                })
-            })
-            .collect::<Vec<_>>();
+                    .collect::<Vec<_>>();
 
-        assert!(
-            !(without_char > 0 && without_char != tiles.len()),
-            "Either all or no tiles must have 'char' attribute"
-        );
+                let mut unique_chars = HashSet::new();
+                let mut duplicated_span = None;
+                let char_to_tiles = tiles
+                    .iter()
+                    .filter_map(|tile| {
+                        tile.tile_char.map(|(c, c_span)| {
+                            if !unique_chars.insert(c) {
+                                duplicated_span = Some(c_span);
+                            }
+                            let constr = &tile.ident;
+                            Some(quote! {
+                                #c => ::core::option::Option::Some(#tile_enum_name::#constr)
+                            })
+                        })
+                    })
+                    .collect::<Vec<_>>();
 
-        // don't generate CharTile implementation
-        if without_char == tiles.len() {
-            None
-        } else {
-            Some(quote! {
-            #[automatically_derived]
-            impl #cgt_crate::grid::CharTile for #tile_enum_name {
-                #[inline]
-                fn tile_to_char(self) -> char {
-                    match self {
-                        #(#tile_to_chars),*
+                if !without_char.is_empty() && without_char.len() != tiles.len() {
+                    return quote_spanned! { without_char[0] =>
+                        #dummy_impls
+                        compile_error!("Either all or no tiles must have '#[tile(char(...))]' attribute");
                     }
+                    .into();
                 }
 
-                #[inline]
-                fn char_to_tile(input: char) -> Option<Self> {
-                    match input {
-                        #(#char_to_tiles),*
-                        ,_ => ::core::option::Option::None,
+                if let Some(duplicated_span) = duplicated_span {
+                    return quote_spanned! { duplicated_span =>
+                        #dummy_impls
+                        compile_error!("Every '#[tile(char(...))]' attribute must be unique");
                     }
+                    .into();
                 }
-            }
-            })
-        }
-    };
 
-    let impl_char_bool = {
-        let mut with_bool = 0;
-        let mut case_true = None;
-        let mut case_false = None;
-
-        for tile in &tiles {
-            let constr = &tile.ident;
-
-            if let Some(b) = tile.tile_bool {
-                with_bool += 1;
-
-                if b {
-                    match case_true {
-                        Some(_) => panic!("Only one tile can be 'bool(true)'"),
-                        None => case_true = Some(quote! { #tile_enum_name::#constr }),
-                    }
+                // don't generate CharTile implementation
+                if without_char.len() == tiles.len() {
+                    None
                 } else {
-                    match case_false {
-                        Some(_) => panic!("Only one tile can be 'bool(false)'"),
-                        None => case_false = Some(quote! { #tile_enum_name::#constr }),
+                    Some(quote! {
+                    #[automatically_derived]
+                    impl #cgt_crate::grid::CharTile for #tile_enum_name {
+                        #[inline]
+                        fn tile_to_char(self) -> char {
+                            match self {
+                                #(#tile_to_chars),*
+                            }
+                        }
+
+                        #[inline]
+                        fn char_to_tile(input: char) -> Option<Self> {
+                            match input {
+                                #(#char_to_tiles),*
+                                ,_ => ::core::option::Option::None,
+                            }
+                        }
+                    }
+                    })
+                }
+            };
+
+            let impl_char_bool = {
+                let mut without_bool = Vec::new();
+                let mut case_true = None;
+                let mut case_false = None;
+
+                for tile in &tiles {
+                    let constr = &tile.ident;
+
+                    match tile.tile_bool {
+                        Some((b, span)) => {
+                            if b {
+                                match case_true {
+                                    Some(_) => {
+                                        return quote_spanned! { span =>
+                                            #dummy_impls
+                                            compile_error!("Only one tile can be 'bool(true)'");
+                                        }
+                                        .into();
+                                    }
+                                    None => case_true = Some(quote! { #tile_enum_name::#constr }),
+                                }
+                            } else {
+                                match case_false {
+                                    Some(_) => {
+                                        return quote_spanned! { span =>
+                                            #dummy_impls
+                                            compile_error!("Only one tile can be 'bool(false)'");
+                                        }
+                                        .into();
+                                    }
+                                    None => case_false = Some(quote! { #tile_enum_name::#constr }),
+                                }
+                            }
+                        }
+                        None => without_bool.push(constr.span()),
                     }
                 }
+
+                if without_bool.len() == tiles.len() {
+                    None
+                } else if !without_bool.is_empty() {
+                    return quote_spanned! { without_bool[0] =>
+                            #dummy_impls
+                            compile_error!("Either all or no tiles must have '#[tile(bool(...))]' attribute");
+                        }
+                        .into();
+                } else {
+                    let case_true = case_true.unwrap();
+                    let case_false = case_false.unwrap();
+
+                    Some(quote! {
+                        #[automatically_derived]
+                        impl #cgt_crate::grid::BitTile for #tile_enum_name {
+                            #[inline]
+                            fn tile_to_bool(self) -> bool {
+                                match self {
+                                    #case_true => true,
+                                    #case_false => false,
+                                }
+                            }
+
+
+                            #[inline]
+                            fn bool_to_tile(input: bool) -> Self {
+                                if input {
+                                    #case_true
+                                } else {
+                                    #case_false
+                                }
+                            }
+                        }
+                    })
+                }
+            };
+
+            quote! {
+                #impl_default
+                #impl_char_tile
+                #impl_char_bool
             }
+            .into()
         }
-
-        if with_bool == 0 {
-            None
-        } else {
-            assert!(
-                with_bool == tiles.len(),
-                "Either all or no tiles must have 'bool' attribute"
-            );
-
-            let case_true = case_true.unwrap();
-            let case_false = case_false.unwrap();
-
-            Some(quote! {
-                #[automatically_derived]
-                impl #cgt_crate::grid::BitTile for #tile_enum_name {
-                    #[inline]
-                    fn tile_to_bool(self) -> bool {
-                        match self {
-                            #case_true => true,
-                            #case_false => false,
-                        }
-                    }
-
-
-                    #[inline]
-                    fn bool_to_tile(input: bool) -> Self {
-                        if input {
-                            #case_true
-                        } else {
-                            #case_false
-                        }
-                    }
-                }
-            })
-        }
-    };
-
-    quote! {
-        #impl_default
-        #impl_char_tile
-        #impl_char_bool
     }
-    .into()
 }
