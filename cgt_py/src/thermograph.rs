b7bd96004eff196e089131c14c39d7b2c17e91dc
t4ccer <t4ccer@gmail.com>
2026-06-06 11:39:47 -0230

    Restore thermograph visualization in python

--- cgt_py/src/thermograph.rs
+++ cgt_py/src/thermograph.rs
@@ -1,4 +1,7 @@
-use cgt::short::partizan::thermograph::Thermograph;
+use cgt::{
+    drawing::{Draw, svg},
+    short::partizan::thermograph::Thermograph,
+};
 use pyo3::prelude::*;
 
 crate::wrap_struct!(Thermograph, PyThermograph, "Thermograph", Clone);
@@ -7,5 +10,12 @@ crate::wrap_struct!(Thermograph, PyThermograph, "Thermograph", Clone);
 impl PyThermograph {
     fn __repr__(&self) -> String {
         format!("Thermograph({})", self.inner)
+    }
+
+    fn _repr_svg_(&self) -> String {
+        let bb = self.inner.required_canvas::<svg::Canvas>();
+        let mut canvas = svg::Canvas::new(bb);
+        self.inner.draw(&mut canvas);
+        canvas.to_svg()
     }
 }
